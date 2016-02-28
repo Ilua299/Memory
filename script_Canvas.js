9d474baaaf009b964e326edf641301d2747d0c64
Ilua299 <ilua299@gmail.com>
2016-02-28 13:20:31 +0300

    Update script_Canvas.js

--- script_Canvas.js
+++ script_Canvas.js
@@ -1,8 +1,9 @@
-﻿var fps = 30;
+var fps = 30;
 canvas = document.getElementById("Canvas");
 context = canvas.getContext("2d");
 var img = new Image();
 img.src = "img/cartes.png";
+var Number = 0;//Подсчет нажатий на канвас
 
 var cards  = {
     0:  { x : 0, y : 0 },
@@ -49,38 +50,28 @@ for (var i = 1; i <= 16; i++){
 randomCardPlaces.sort(function() {return 0.5 - Math.random()});
 
 
-img.onload = function ()
-{    
-    for (var i = 0; i < 16; i++)
-    {
-      context.drawImage(img, cards[i].x, cards[i].y, 191, 280, randomCardPlaces[i].x, randomCardPlaces[i].y, 190, 280); 
-      context.drawImage(img, 390, 1120, 191, 280, randomCardPlaces[i].x, randomCardPlaces[i].y, 190, 280);
-    };
-} 
 
-
-/*var CARD_OPENED_STATE = 1;
-var CARD_CLOSED_STATE = 0;
+var CARD_OPENED_STATE = 1;
+var CARD_CLOSED_STATE = 1;
 
  
-var CardF = function()
+var CardF = function(x0,y0,i)
 {
-    this.cardCode = 0;
-    this.x = 0;
-    this.y = 0;
+    this.cardCode = i;
+    this.x = x0;
+    this.y = y0;
     this.width = 190;
     this.height = 280;
     this.state = CARD_CLOSED_STATE;
-    this.onClick = function()
+    this.onClick = function(this.x,this,y,this,this.width,this.height,this.state)
     {
-        this.state = CARD_OPENED_STATE;
         if (this.state == CARD_OPENED_STATE)
         {
-          context.drawImage(img, cards[i].x, cards[i].y, this.width, this.height, randomCardPlaces[i].x, randomCardPlaces[i].y, this.width, this.height); 
+          context.drawImage(img, cards[i].x, cards[i].y, this.width, this.height, this.x, this.y, this.width, this.height); 
         }
         else
         {
-          context.drawImage(img, 390, 1120, this.width, this.height, randomCardPlaces[i].x, randomCardPlaces[i].y, this.width, this.height);
+          context.drawImage(img, 390, 1120, this.width, this.height, this.x, this.y, this.width, this.height);
         }        
     }
 }  
@@ -90,37 +81,21 @@ var CardF = function()
 
     for (var i = 0; i < 8; i++)
     {
-        var card1 = CardF();
-        card1.x = randomCardPlaces[i].x;
-        card1.y = randomCardPlaces[i].y;
-        card1.cardCode = i;
-        card1.onClick();
+        var card1 = CardF(randomCardPlaces[i].x, randomCardPlaces[i].y, i);
 		
-        var card2 = new CardF();
-        card2.x = randomCardPlaces[15 - i].x;
-        card2.y = randomCardPlaces[15 - i].y;
-        card2.cardCode = i;
-	    card2.onClick();
+        var card2 = new CardF(randomCardPlaces[15 - i].x, randomCardPlaces[15 - i].y, i);
 		
         Cardss.push(card1);
         Cardss.push(card2);        
-    };    */
- /*   
-    var clickX = "";
-    for (var i = 0; i < 8; i++)
-    {        
-        var card = cards[i];
-        if (true)
-        {
-            card.onClick();
-        }
     };
-*/    
+	
+	
 canvas.onclick = function() {
 	var mouseX =  event.pageX - canvas.offsetLeft;
 	var mouseY =  event.pageY - canvas.offsetTop;
 	console.log(mouseX);
 	console.log(mouseY);
+	Number++;
 	for (var i = 0; i < 16;i++){
 	    if ((mouseX > Cardss[i].x) && (mouseX < (Cardss[i].x + 190)) && (mouseY > Cardss[i].y) && (mouseY < (Cardss[i].y + 280)))
 		{
@@ -128,43 +103,8 @@ canvas.onclick = function() {
 		}
 	}
 }
-    
-    
-    
-    
-/*
-var x = 1000;
-var y = 200;
-var radius = 100;
-var rotate = false;
-var startAngle = 10*Math.PI/7;
-var endAngle = 2*Math.PI;
-function gameLoop()
-{ 
-	context.lineWidth = 8;
-    context.strokeStyle = 'rgba(204,0,65,1)';
-    var time = (new Date().getTime()- startTime)/ duration;
-	endAngle = startAngle +(endAngle - startAngle)* time;
-    update(time);
-    render();
-}
 
-function update (delta)
-{
-     endAngle = startAngle +(endAngle - startAngle)* delta;
-}
 
-function render()
-{
-        context.beginPath();
-        context.arc(x, y, radius, startAngle, endAngle, rotate);
-        context.stroke();
-}
-var duration = 30000,
-startTime = new Date().getTime();
-var gameInterval = setInterval(gameLoop,1000/fps); 
-
-*/
 var startAngle = 10 * Math.PI / 7;
 var endAngle = 2 * Math.PI / 1.0;
 var x = 1000;
